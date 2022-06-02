2abe535da5d3567f8ce001affaaf9d4e1392d717
chaddad23 <105995243+chaddad23@users.noreply.github.com>
2022-06-02 13:36:49 -0700

    Add files via upload

---
+++ src/main/java/com/gof/hr2s/user/Clerk.java
@@ -0,0 +1,39 @@
+package com.gof.hr2s.user;
+
+public class Clerk extends User{
+
+    private String clerkFirstName;
+    private String clerkLastName;
+    public String jobTitle;
+
+    public Clerk(int userId, Account accountType, String username, String password, String firstName, String lastName, String jobTitle) {
+        super(userId, Account.CLERK, username, password, firstName, lastName);
+        this.clerkFirstName = firstName;
+        this.clerkLastName = lastName;
+        this.jobTitle = "Hotel Clerk";
+    }
+
+    public String getClerkFirstName() {
+        return clerkFirstName;
+    }
+
+    public void setClerkFirstName(String clerkFirstName) {
+        this.clerkFirstName = clerkFirstName;
+    }
+
+    public String getClerkLastName() {
+        return clerkLastName;
+    }
+
+    public void setClerkLastName(String clerkLastName) {
+        this.clerkLastName = clerkLastName;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+}
