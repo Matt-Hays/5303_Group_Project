e1d3cc072f52e7b58f1de61d944979b0141256aa
joshuasouthern <joshuasouthern92@gmail.com>
2022-06-04 21:52:43 -0700

    admin_class_made

---
+++ src/main/java/com/gof/hr2s/user/Admin.java
@@ -0,0 +1,28 @@
+package com.gof.hr2s.user;
+
+import com.gof.hr2s.db.Database;
+
+public class Admin extends User{
+
+    private String adminFirstName;
+    private String adminLastName;
+    public String jobTitle;
+
+
+    public Admin(int userId, Account accountType, String username, String firstName, String lastName, String jobTitle) {
+        super(userId, accountType.ADMIN, username, firstName, lastName);
+        this.adminFirstName = firstName;
+        this.adminLastName = lastName;
+        this.jobTitle = "Admin";
+    }
+
+    public void createClerk(int newUserId, Account newAccountType, String newUsername, String newFirstName, String newLastName, String newJobTitle){
+        this.userId = newUserId;
+        this.accountType = newAccountType;
+        this.username = newUsername;
+        this.firstName = newFirstName;
+        this.lastName = newLastName;
+        this.jobTitle = newJobTitle;
+
+    }
+}
