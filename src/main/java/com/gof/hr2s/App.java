fad23b6f4ebbf75b877a07910f40a70f821fbf94
Matthew Hays <hays8786@gmail.com>
2022-07-12 00:16:15 -0400

    Adjust Function Calls

--- src/main/java/com/gof/hr2s/App.java
+++ src/main/java/com/gof/hr2s/App.java
@@ -1,6 +1,7 @@
 package com.gof.hr2s;
 
 import com.gof.hr2s.controller.AppController;
+import com.gof.hr2s.database.Database;
 import com.gof.hr2s.service.HotelModels;
 import com.gof.hr2s.ui.HotelViews;
 
@@ -12,7 +13,8 @@ public class App {
         // Models
         HotelModels models = new HotelModels();
         // Persistence Layer
+        Database db = Database.Database();
         // Controller
-        AppController controller = new AppController(models, views);
+        AppController controller = new AppController(models, views, db);
     }
 }
--- src/main/java/com/gof/hr2s/service/HotelModels.java
+++ src/main/java/com/gof/hr2s/service/HotelModels.java
@@ -2,20 +2,24 @@ package com.gof.hr2s.service;
 
 import com.gof.hr2s.models.Reservation;
 import com.gof.hr2s.models.Room;
+import com.gof.hr2s.models.Session;
 import org.springframework.security.core.userdetails.User;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class HotelModels {
     private static ReservationCatalog reservationCatalog;
     private static RoomCatalog roomCatalog;
     private static UserCatalog userCatalog;
+    private static SessionCatalog sessionCatalog;
 
     public HotelModels() {
         this.reservationCatalog = ReservationCatalog.getReservationCatalog();
         this.roomCatalog = RoomCatalog.getRoomCatalog();
         this.userCatalog = UserCatalog.getUserCatalog();
+        this.sessionCatalog = SessionCatalog.getSessionCatalog();
     }
 
     public static ArrayList<Room> searchAvailableRooms(LocalDate arrival, LocalDate departure) {
@@ -24,5 +28,14 @@ public class HotelModels {
 
     public static Object getUserByUsernameCatalog(String username){
         return userCatalog.getUserByUsername(username);
+    }
+
+    public static UUID createNewSession(Object user){
+        return sessionCatalog.createSession(user);
+    }
+
+    public static void createGuest(String username, String password, String firstName, String lastName,
+                                   String address1, String address2, String city, String state, String zip){
+        userCatalog.createNewGuest(username, password, firstName, lastName, address1, address2, city, state, zip);
     }
 }
