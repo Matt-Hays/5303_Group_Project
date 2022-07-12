8a4a7ff173114cebcd8e79309b50ce77eb222bcc
joshuasouthern <joshuasouthern92@gmail.com>
2022-07-11 18:36:05 -0700

    added admin, guest, user catalog classes and updated clerk and user

---
+++ src/main/java/com/gof/hr2s/service/UserCatalog.java
@@ -0,0 +1,39 @@
+package com.gof.hr2s.service;
+
+import com.gof.hr2s.database.Database;
+import com.gof.hr2s.models.Guest;
+import com.gof.hr2s.models.Reservation;
+import com.gof.hr2s.models.ReservationStatus;
+import com.gof.hr2s.models.Room;
+
+import java.time.LocalDate;
+
+public class UserCatalog {
+    public static UserCatalog userCatalog = null;
+
+    private Database db = null;
+
+    private UserCatalog() {
+        db = Database.Database();
+    }
+
+    public static UserCatalog getUserCatalog() {
+        if(null == userCatalog) {
+            userCatalog = new UserCatalog();
+        }
+
+        return userCatalog;
+    }
+
+    public Object getUserByUsername(String username) {
+        return db.getUser(username);
+    }
+
+    public Object getUserById(UUID userId) {
+        return db.getUser(userId);
+    }
+
+    public ArrayList<Object> getAllUsers() {
+        return db.getAllUsers();
+    }
+}
