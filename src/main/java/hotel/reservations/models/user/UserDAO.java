0d74f0128bb637a3fb4f30b5313de79bc04d96be
chaddad23 <105995243+chaddad23@users.noreply.github.com>
2022-07-20 10:37:15 -0700

    No more red squigglies

--- src/main/java/hotel/reservations/models/user/UserDAO.java
+++ src/main/java/hotel/reservations/models/user/UserDAO.java
@@ -1,6 +1,57 @@
 package hotel.reservations.models.user;
 
+import java.util.UUID;
+
 public class UserDAO implements IUserDAO{
 
 
+    @Override
+    public UUID getUserId() {
+        return null;
+    }
+
+    @Override
+    public Account getAccountType() {
+        return null;
+    }
+
+    @Override
+    public void setUsername(String username) {
+
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public String getFirstName() {
+        return null;
+    }
+
+    @Override
+    public void setFirstName(String firstName) {
+
+    }
+
+    @Override
+    public String getLastName() {
+        return null;
+    }
+
+    @Override
+    public void setLastName(String lastName) {
+
+    }
+
+    @Override
+    public boolean getActive() {
+        return false;
+    }
+
+    @Override
+    public void setActive(boolean active) {
+
+    }
 }
