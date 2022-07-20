48dfa4d12d4474e0b955145f92c1bdb7245f85c9
chaddad23 <105995243+chaddad23@users.noreply.github.com>
2022-07-20 11:42:12 -0700

    Adding additional method signatures to DAO interface

--- src/main/java/hotel/reservations/models/user/IUserDAO.java
+++ src/main/java/hotel/reservations/models/user/IUserDAO.java
@@ -1,5 +1,10 @@
 package hotel.reservations.models.user;
 
+import hotel.reservations.models.reservation.Reservation;
+import hotel.reservations.models.room.Room;
+import hotel.reservations.services.Response;
+
+import java.time.LocalDate;
 import java.util.UUID;
 
 public interface IUserDAO {
@@ -23,4 +28,12 @@ public interface IUserDAO {
     public boolean getActive();
 
     public void setActive(boolean active);
+
+    public Response changePassword(String username, String currentPassword, String newPassword);
+
+    public Reservation createReservation(LocalDate arrival, LocalDate departure, Room room);
+
+    public Reservation createReservation(LocalDate arrival, LocalDate departure, int roomId);
+
+    public Response setCustomer(Guest guest);
 }
--- src/main/java/hotel/reservations/models/user/UserDAO.java
+++ src/main/java/hotel/reservations/models/user/UserDAO.java
@@ -1,8 +1,20 @@
 package hotel.reservations.models.user;
 
+import hotel.reservations.models.reservation.Reservation;
+import hotel.reservations.models.room.Room;
+import hotel.reservations.persistence.Database;
+import hotel.reservations.services.Response;
+import hotel.reservations.services.UserCatalog;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.time.LocalDate;
 import java.util.UUID;
 
 public class UserDAO implements IUserDAO{
+
+    private Database db = null;
+    private UserDAO() { db = Database.Database(); }
 
 
     @Override
@@ -53,5 +65,30 @@ public class UserDAO implements IUserDAO{
     @Override
     public void setActive(boolean active) {
 
+    }
+
+    @Override
+    public Response changePassword(String username, String currentPassword, String newPassword) {
+        try {
+            return db.updatePassword(username, currentPassword, newPassword);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+        }
+
+        return Response.FAILURE;
+    }
+
+    @Override
+    public Reservation createReservation(LocalDate arrival, LocalDate departure, Room room) {
+        return null;
+    }
+
+    @Override
+    public Reservation createReservation(LocalDate arrival, LocalDate departure, int roomId) {
+        return null;
+    }
+
+    @Override
+    public Response setCustomer(Guest guest) {
+        return null;
     }
 }
