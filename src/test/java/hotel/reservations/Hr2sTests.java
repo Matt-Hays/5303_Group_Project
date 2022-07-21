3c05d367e42919b7fcc8759a18d3093597ad6a40
Ron Wellman <ron.wellman01@gmail.com>
2022-07-21 09:37:53 -0400

    Allow db to persist and add sample tests.

--- src/test/java/hotel/reservations/Hr2sTests.java
+++ src/test/java/hotel/reservations/Hr2sTests.java
@@ -1,23 +1,34 @@
 package hotel.reservations;
 
+import hotel.reservations.models.user.Account;
+import hotel.reservations.models.user.Admin;
+import hotel.reservations.models.user.Clerk;
+import hotel.reservations.models.user.Guest;
 import hotel.reservations.persistence.Database;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import hotel.reservations.services.*;
+import hotel.reservations.services.authentication.HotelAuth;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import java.io.File;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@TestMethodOrder(OrderAnnotation.class)
 class Hr2sTests {
-    private final String dbName = "testDB.sqlite";
-    Database db = null;
+    private static final String dbName = "testDB.sqlite";
+    private static Database db = null;
 
     /**
      * Ensures database is created and in a clean state before each test
      */
-    @BeforeEach
-    void setup() {
+    @BeforeAll
+    static void setup() {
         File dbFile = new File(dbName);
         // delete the current database if it exists
         if (dbFile.exists()) {
@@ -31,13 +42,8 @@ class Hr2sTests {
     /**
      * Ensures the database does not persist between tests
      */
-    @AfterEach
-    void teardown() {
-        // close the existing database
-        if (null != db) {
-            db.close();
-        }
-
+    @AfterAll
+    static void teardown() {
         File dbFile = new File(dbName);
         // delete the current database if it exists
         if (dbFile.exists()) {
@@ -45,8 +51,52 @@ class Hr2sTests {
         }
     }
 
+    /**
+     * test whether the database is ready
+     */
     @Test
+    @Order(1)
     void testDatabaseReady() {
         assertTrue(db.ready());
+    }
+
+    /**
+     * test retrieval of a user
+     */
+    @Test
+    @Order(2)
+    void getAdminUser() {
+        Object obj = db.getUser("admin");
+        assertTrue(obj instanceof Admin);
+    }
+
+    @Test
+    @Order(3)
+    void registerUser() throws NoSuchAlgorithmException, InvalidKeySpecException {
+        UserCatalog uc = UserCatalog.getUserCatalog();
+        uc.createNewGuest(Account.GUEST, "guest1", HotelAuth.generatePasswordHash("password123$"), "bob", "thebuilder", true);
+    }
+
+    /**
+     * test retrieval of a guest user
+     */
+    @Test
+    @Order(4)
+    void getGuestUser() {
+        Object obj = db.getUser("guest1");
+        assertTrue(obj instanceof Guest);
+    }
+
+    /**
+     * creation of a clerk
+     */
+    @Test
+    @Order(5)
+    void createClerk() {
+        Object obj = db.getUser("admin");
+        assertTrue(obj instanceof Admin);
+
+        Admin admin = (Admin)obj;
+        assertEquals(Response.SUCCESS, admin.createUser(Account.CLERK, "clerk1", "Clerky", "McClerk"));
     }
 }
