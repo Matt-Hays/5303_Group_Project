a7cfb273395d62053f9aeefd30285a51a139e694
chaddad23 <105995243+chaddad23@users.noreply.github.com>
2022-07-21 10:19:11 -0700

    Additional refactoring and moving database functions to the DAO implementation

--- src/main/java/hotel/reservations/models/user/IUserDAO.java
+++ src/main/java/hotel/reservations/models/user/IUserDAO.java
@@ -8,32 +8,15 @@ import java.time.LocalDate;
 import java.util.UUID;
 
 public interface IUserDAO {
-
-    public UUID getUserId();
-
-    public Account getAccountType();
-
-    public void setUsername(String username);
-
-    public String getUsername();
-
-    public String getFirstName();
-
-    public void setFirstName(String firstName);
-
-    public String getLastName();
-
-    public void setLastName(String lastName);
-
-    public boolean getActive();
-
-    public void setActive(boolean active);
-
     public Response changePassword(String username, String currentPassword, String newPassword);
 
-    public Reservation createReservation(LocalDate arrival, LocalDate departure, Room room);
+    public Reservation createReservation(LocalDate arrival, LocalDate departure, Room room, User user);
 
-    public Reservation createReservation(LocalDate arrival, LocalDate departure, int roomId);
+    public Reservation createReservation(LocalDate arrival, LocalDate departure, int roomId, User user);
 
     public Response setCustomer(Guest guest);
+
+    public Response updateUser();
+
+
 }
