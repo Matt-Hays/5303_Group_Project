4c2ca0bfcf1b60534ce79c2b2f36c47d9b5d21b6
ssfaron <slsmallw.asu@gmail.com>
2022-07-22 23:25:40 -0500

    add invoice dao

---
+++ src/main/java/hotel/reservations/services/invoiceDAO/invoiceDAOImpl.java
@@ -0,0 +1,26 @@
+package hotel.reservations.services.invoiceDAO;
+
+import hotel.reservations.persistence.Database;
+
+public class invoiceDAOImpl extends reservationDAOImpl {
+    private static invoiceDAOImpl dao = null;
+    private static Database db = null;
+
+    private invoiceDAOImpl() {
+    }
+
+    public static invoiceDAOImpl getInvoiceDAO() {
+        if (null == dao) {
+            dao = new invoiceDAOImpl();
+            db = Database.Database();
+        }
+
+        return dao;
+    }
+
+    // getInvoice
+    // createInvoice
+    // updateInvoice
+    // deleteInvoice
+}
+
