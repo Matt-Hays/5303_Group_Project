cea3b5ec3d2bfa81a3423ece5a34ed7d8eb77656
Ron Wellman <ron.wellman01@gmail.com>
2022-07-25 01:03:32 -0400

    Adjust Room prototype.

--- src/main/java/hotel/reservations/services/roomDAO/IRoomDAO.java
+++ src/main/java/hotel/reservations/services/roomDAO/IRoomDAO.java
@@ -7,7 +7,7 @@ import hotel.reservations.services.Response;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
-public interface IRoomDAO<Room> {
+public interface IRoomDAO {
     public ArrayList<Room> filterRooms(LocalDate arrival, LocalDate departure);
     public hotel.reservations.models.room.Room getRoom(String roomId);
     public ArrayList<hotel.reservations.models.room.Room> getAllRooms();
