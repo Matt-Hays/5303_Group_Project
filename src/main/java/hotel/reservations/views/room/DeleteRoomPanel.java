6696efd704b1ee0f22abb0c2f595988d0fe351c8
Matthew Hays <hays8786@gmail.com>
2022-08-07 18:35:20 -0400

    Add documentation in DeleteRoomPanel

--- src/main/java/hotel/reservations/views/room/DeleteRoomPanel.java
+++ src/main/java/hotel/reservations/views/room/DeleteRoomPanel.java
@@ -1,3 +1,10 @@
+/**
+ * @file DeleteRoomPanel.java
+ * @author Sarah Smallwood
+ * @brief Unimplemented - For future extension.
+ * @dependencies none - Frame.java interface.
+ */
+
 package hotel.reservations.views.room;
 
 public class DeleteRoomPanel {
